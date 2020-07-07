f8c737640cb76f0bbe7e6cfbbc60f858cc4bb553
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-07 15:58:44 -0500

    commit 1: added Employee Class

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,14 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name,id,email) { 
+        this.name = name;
+        this.id = id;
+        this.email = email;
+                         }
+
+    getName() { return this.name }
+    getId()   { return this.id }
+    getEmail() {return this.email}
+    getRole() { var Employee = "Employee"; return Employee }
+}
+module.exports = Employee
