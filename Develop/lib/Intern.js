def33ce6463a06cc11d57ffba82461c4717c0637
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-12 00:01:14 -0500

    commit 9: cleaned up Intern Class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,9 +1,20 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+/*
+I. Dependecies:
+    A. ' Employee ' Class
+II. ' Engineering ' Class extension:
+    A. super - constructor
+        1. ' school ', school of Intern
+    B. methods ()
+        1. getRole(), returns Employee type 
+        getSchool(), returns school of Employee
+
+*/ 
+
 const Employee = require('./Employee.js')
 
 class Intern extends Employee {
     constructor(name,id,email, school) 
-{       super(name, id, email, school)
+{       super(name, id, email)
         this.school = school;
 }
 
