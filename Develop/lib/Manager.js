a42799ff38d02fd423eeb795d67f87ca95dbb1a0
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-12 00:28:45 -0500

    commit 11: cleaned up Manager Class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,26 +1,24 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+/*
+I. Dependecies:
+    A. 'Employee' Class
+II. 'Engineer' Class extension
+    A. super - constructor
+        1. officeNumber, manager's phone number
+    B. methods:
+        1. getRole(), returns employee's role
+        2. getOfficeNumber(), returns officeNumber 
+*/
 const Employee = require('./Employee.js');
 
-class Manager extends Employee{
-    constructor(name,id,email, getRole, officeNumber) 
-{       super(Employee)
-        this.name = name; 
-        this.id = id; 
-        this.email = email; 
-        this.getRole= getRole; 
-        this.officeNumber = officeNumber;
-
-        
-        this.Employee = "Manager";
-        this.getRole = "Intern";
-        this.officeNumber = 100;
-        this.name = Employee.prototype.getName;
-        this.id = Employee.prototype.getId;
-        this.getRole = Manager.prototype.getRole;
-        this.email = Employee.prototype.getEmail;  
+class Manager extends Employee {
+    constructor(name,id,email, officeNumber) 
+{       super(name, id, email)
+        this.officeNumber = officeNumber; 
 }
 
-getRole() { return this.Employee}
-getOfficeNumber() {return this.officeNumber}
+getRole() { return 'Manager'}
+getOfficeNumber() { return this.officeNumber}
+
 }
-module.exports = Manager
+
+module.exports = Manager;
