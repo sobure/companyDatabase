98c43e8177dde712a685f8701ad700f49dd3d845
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-13 07:52:19 -0500

    commit 12: express modules

--- Develop/app.js
+++ Develop/app.js
@@ -6,34 +6,30 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-var app = express();
-var PORT = 8080
-
 const teamMembers = [];
 const idArray = [];
 
-
+appMenu()
 function appMenu() {
 
+    createManager()
     function createManager () {
     
-    console.log("Please build your team");
+    console.log('Team Assembly:');
     inquirer.prompt([ {
-[
-    {
         type: "input",
         name: "managerName", 
-        message: "What is your Manager's name?" ,
-        validate: answer => { 
-        if (answer !=="" ){ r
-            return true;
-        }
-         return "Please enter at least one character.";
-    },
+        message: "Enter name?" ,
+        validate: answer => 
+        { 
+        if (answer !== "" ) { return true; }
+         return "Please enter at least one character ." }
+    }
+,
     {
         type: 'input',
         name: 'managerId',
-        message: 'what is your managers id?',
+        message: 'Enter manager id?',
         validate: answer => {
             const pass = answer.
             match(
@@ -43,63 +39,52 @@ function appMenu() {
             return true;
         }
         return "Please enter a positve number greater than zero.";
-        )
-    }   } 
-    {type: "input"
-     name: "managerId",
-    message: "What is your manager's id? "} , 
-    validate: answer => { 
+    }  
+ } ,
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter managers email? ", 
+        validate: answer => { 
         const pass = answer.
-        match(/\+@\S+\,\[1-9]\d*$/);
+        match(
+            /\S+@\S+\.\S+/
+            );
         if (pass) {
             return true;
         }
-    }
-    return "Please enter a positiev number greater than zero."; 
-}
+        return "Pleae enter a valid email address.";}
+
     },
-
-
-    validate: if (answer !== "") {
-        return true;
+    {
+     type: "input",
+     name: "managerOfficeNumber",
+     message: "What is your manger's office number?",
+     validate: answer => {
+         const pass = answer.match(
+             /^[1-9]\d*$/
+         );
+         if (pass) {
+             return true;
+         }
+         return "Please enter a positive number greater than zero."; 
+      }
     }
-    return "Please enter at leaswt one character";
-}}}]
+]).then(answers => {
+    const manager = new Manager(answers.managName, answers.managerID, answers.mangerEmail, answers.managerOfficeNumber); teamMembers.push(manager);
+    idArray.push(answers.managerId);
+    console.log(idArray)
+    //createTeam();
+});
+    }
+}
 
-app.use(express.urlencoded({extend: true}));
-app.use(express.json())
+//app.use(express.urlencoded({extend: true}));
+//app.use(express.json())
 
 // Displays the team
-app.get("/main", function(req, res){
-    res.sendFile(path.join(__dirname,"main.html"));
-})
-app.get("/team")
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const 
-
-const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//app.get("/main", function(req, res){
+  //  res.sendFile(path.join(__dirname,"main.html"));})
+// app.get("/team")
+//const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html") */
---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+
+// Dependencies
+var express = require ("express");
+var path = require("path");
+var Employee = "req" 
+var Engineer = 
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+
+var server
